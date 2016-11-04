28ff9a7e9b33ac018e0fcd61b9257904bd226584
Taras Sulyatytskiy <taras.suliatitskiy@blinkreaction.com>
2016-11-04 15:20:20 +0200

    first commit

---
+++ docroot/vendor/drupal/console/bin/drupal.php
@@ -0,0 +1,123 @@
+<?php
+
+use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Drupal\Console\Utils\ConfigurationManager;
+use Drupal\Console\Utils\ArgvInputReader;
+use Drupal\Console\Bootstrap\Drupal;
+use Drupal\Console\Application;
+
+
+set_time_limit(0);
+$appRoot = getcwd() . '/';
+$root = $appRoot;
+
+$globalAutoLoadFile = $appRoot.'/autoload.php';
+$projectAutoLoadFile = $appRoot.'/vendor/autoload.php';
+
+if (file_exists($globalAutoLoadFile)) {
+    $autoload = include_once $globalAutoLoadFile;
+} elseif (file_exists($projectAutoLoadFile)) {
+    $autoload = include_once $projectAutoLoadFile;
+} else {
+    echo PHP_EOL .
+        ' DrupalConsole must be executed within a Drupal Site.'.PHP_EOL.
+        ' Try changing to a Drupal site directory and download it by executing:'. PHP_EOL .
+        ' composer require drupal/console:~1.0 --prefer-dist --optimize-autoloader'. PHP_EOL .
+        ' composer update drupal/console --with-dependencies'. PHP_EOL .
+        PHP_EOL;
+
+    exit(1);
+}
+
+if (!file_exists($appRoot.'composer.json')) {
+    $root = realpath($appRoot . '../') . '/';
+}
+
+if (!file_exists($root.'composer.json')) {
+    echo ' No composer.json file found at:' . PHP_EOL .
+         ' '. $root . PHP_EOL .
+         ' you should try run this command,' . PHP_EOL .
+         ' from the Drupal root directory.' . PHP_EOL;
+
+    exit(1);
+}
+
+/* relocate to a class */
+$today = date('Y-m-d');
+$loggerFile = $root.'console/log/' . $today . '.log';
+$handle = null;
+
+if (!is_file($loggerFile)) {
+    try {
+        $directoryName = dirname($loggerFile);
+        if (!is_dir($directoryName )) {
+            mkdir($directoryName, 0777, TRUE);
+        }
+        touch($loggerFile);
+    } catch (\Exception $e) {
+        $loggerFile = null;
+        $loggerOutput = new ConsoleOutput();
+    }
+}
+if ($loggerFile && is_writable($loggerFile)) {
+    try {
+        $handle = fopen($loggerFile, 'a+');
+        $loggerOutput = new StreamOutput($handle);
+    } catch (\Exception $e) {
+        $loggerOutput = new ConsoleOutput();
+    }
+} else {
+    $loggerOutput = new ConsoleOutput();
+}
+/* relocate to a class */
+
+$argvInputReader = new ArgvInputReader();
+$configurationManager = new ConfigurationManager();
+$configuration = $configurationManager->loadConfiguration($root)
+    ->getConfiguration();
+if ($options = $configuration->get('application.options') ?: []) {
+    $argvInputReader->setOptionsFromConfiguration($options);
+}
+$argvInputReader->setOptionsAsArgv();
+
+if ($root === $appRoot && $argvInputReader->get('root')) {
+    $appRoot = $argvInputReader->get('root');
+    if (is_dir($appRoot)) {
+        chdir($appRoot);
+    }
+    else {
+        $appRoot = $root;
+    }
+}
+
+$drupal = new Drupal($autoload, $root, $appRoot, $loggerOutput);
+$container = $drupal->boot();
+
+/* relocate to a class */
+if ($handle) {
+    fclose($handle);
+}
+/* relocate to a class */
+
+if (!$container) {
+    echo ' In order to list all of the available commands you should try: ' . PHP_EOL .
+         ' Copy config files: drupal init ' . PHP_EOL .
+         ' Install Drupal site: drupal site:install ' . PHP_EOL;
+
+    exit(1);
+}
+
+$configuration = $container->get('console.configuration_manager')
+    ->getConfiguration();
+
+$translator = $container->get('console.translator_manager');
+
+if ($options = $configuration->get('application.options') ?: []) {
+    $argvInputReader->setOptionsFromConfiguration($options);
+}
+$argvInputReader->setOptionsAsArgv();
+
+$application = new Application($container);
+$application->setDefaultCommand('about');
+$application->run();
---
+++ docroot/vendor/drupal/console/src/Bootstrap/Drupal.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Drupal\Console\Bootstrap;
+
+use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\Console\Utils\ArgvInputReader;
+
+class Drupal
+{
+    protected $autoload;
+    protected $root;
+    protected $appRoot;
+    protected $loggerOutput;
+
+    /**
+     * Drupal constructor.
+     * @param $autoload
+     * @param $root
+     * @param $appRoot
+     * @param $loggerOutput
+     */
+    public function __construct($autoload, $root, $appRoot, OutputInterface $loggerOutput)
+    {
+        $this->autoload = $autoload;
+        $this->root = $root;
+        $this->appRoot = $appRoot;
+        $this->loggerOutput = $loggerOutput;
+    }
+
+    public function boot()
+    {
+        if (!class_exists('Drupal\Core\DrupalKernel')) {
+            $this->loggerOutput->writeln('Class Drupal\Core\DrupalKernel not found.');
+            $drupal = new DrupalConsoleCore($this->root, $this->appRoot);
+            return $drupal->boot();
+        }
+
+        try {
+            $argvInputReader = new ArgvInputReader();
+            if ($argvInputReader->get('uri')) {
+              $uri = $argvInputReader->get('uri');
+              if (substr($uri, -1) != '/') {
+                $uri .= '/';
+              }
+              $uri .= 'index.php';
+              $request = Request::create($uri, 'GET', array()  , array(), array(), array('SCRIPT_NAME' => $this->appRoot . '/index.php'));
+            }
+            else {
+              $request = Request::createFromGlobals();
+            }
+
+            $drupalKernel = DrupalKernel::createFromRequest  (
+                $request,
+                $this->autoload,
+                'prod',
+                false
+            );
+
+            $drupalKernel->addServiceModifier(
+                new DrupalServiceModifier(
+                    $this->root,
+                    'drupal.command',
+                    'drupal.generator'
+                )
+            );
+
+            $drupalKernel->invalidateContainer();
+            $drupalKernel->rebuildContainer();
+            $drupalKernel->boot();
+
+            $container = $drupalKernel->getContainer();
+
+            $container->set('console.root', $this->root);
+
+            AnnotationRegistry::registerLoader([$this->autoload, "loadClass"]);
+
+            $configuration = $container->get('console.configuration_manager')
+                ->loadConfiguration($this->root)
+                ->getConfiguration();
+
+            $container->get('console.translator_manager')
+                ->loadCoreLanguage(
+                    $configuration->get('application.language'),
+                    $this->root
+                );
+
+            $container->get('console.renderer')
+                ->setSkeletonDirs(
+                    [
+                        $this->root.DRUPAL_CONSOLE.'/templates/',
+                        $this->root.DRUPAL_CONSOLE_CORE.'/templates/'
+                    ]
+                );
+
+            return $container;
+        } catch (\Exception $e) {
+            $this->loggerOutput->writeln('Error ' . $e->getCode() . ': ' . $e->getMessage());
+            $drupal = new DrupalConsoleCore($this->root, $this->appRoot);
+            return $drupal->boot();
+        }
+    }
+}
